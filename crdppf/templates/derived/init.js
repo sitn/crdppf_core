80bdb1223b9ede057a4994b311b5b488038611e9
Francois Voisard <francois.voisard@ne.ch>
2014-11-17 11:37:16 +0100

    intermediate state of init

--- crdppf/templates/derived/init.js
+++ crdppf/templates/derived/init.js
@@ -1,5 +1,35 @@
 if(!window.Crdppf) Crdppf = {};
 
-//~ labels = ${fr};
-//~ layers = ${layerlist};
+<%
+    counter = 1
+    total = len(fr)
+%>
+    
+Crdppf.labels = {
+    % for key in fr:
+       '${key}' : '${fr[key]}'
+        % if counter < total:
+            ,
+        %endif
+        <%
+            counter += 1
+        %>
+    % endfor
+};
 
+<%
+    j = 1
+    nblayers = len(layerlist)
+%>
+
+Crdppf.layers = [
+    % for layer in layerlist :
+        '${layer}'
+        % if j < nblayers:
+            ,
+        %endif
+        <%
+            j += 1
+        %>
+    % endfor
+];
