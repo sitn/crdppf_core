b6004d88e7ab24757e615e7891f897008ed1dfc3
monodo <monodo@WSSGRF030.ne.ch>
2015-03-31 16:32:09 +0200

    Add missing semicolons

--- crdppf/static/js/Crdppf/helpingFunctions.js
+++ crdppf/static/js/Crdppf/helpingFunctions.js
@@ -7,13 +7,13 @@ var s4 = function () {
   return Math.floor((1 + Math.random()) * 0x10000)
              .toString(16)
              .substring(1);
-}
+};
 
 // Generate UUID V4
 Crdppf.uuid =  function () {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
          s4() + '-' + s4() + s4() + s4();
-}
+};
 
 // Check if and element belongs to a list
 Crdppf.contains = function (element,list){
