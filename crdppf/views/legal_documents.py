300c9f4a02a150194faf4704103d1a7f0243605d
Francois Voisard <francois.voisard@ne.ch>
2015-04-23 14:42:39 +0200

    removed route getLegalbases"

--- crdppf/views/legal_documents.py
+++ crdppf/views/legal_documents.py
@@ -6,7 +6,7 @@ from sqlalchemy import or_
 
 from crdppf.models import DBSession
 from crdppf.models import Topics, Town, OriginReference
-from crdppf.models import Documents, LegalDocuments, LegalBases
+from crdppf.models import Documents, LegalDocuments
 
 @view_config(route_name='getTownList', renderer='json')
 def getTownList(request):
@@ -105,56 +105,6 @@ def createNewDocEntry(request):
     DBSession.flush()
 
     return {'success':True}
-
-@view_config(route_name='getLegalbases', renderer='json')
-def getLegalbases(params):
-    """Gets all the legal bases related to a feature.
-    Input: dict with params : topic, canton, municipalitynb, cadastrenb
-    Output: legalbases
-    """
-
-    doclist = []
-    filterparams = {
-        'request': None,
-        'topic': None,
-        'layer': None,
-        'canton': None,
-        'muncipalitynb': None,
-        'cadastrenb': None
-    }
-            
-    legalbases = {}
-    
-    for param in params:
-        if params[param] is not None and param in filterparams.keys():
-            filterparams[param] = params[param]
-    
-    if param == 'topic':
-        legalbases = DBSession.query(LegalBases).filter_by(topic=params[param]).all()
-    else:
-        sdf
-        legalbases = DBSession.query(LegalBases).order_by(LegalBases.legalbaseid.asc()).all()
-
-    for legalbase in legalbases :
-        doclist.append({
-            legalbase.legalbaseid:{
-                'documentid': legalbase.legalbaseid,
-                'doctype': 'legalbase',
-                #'numcom': legalbase.numcom,
-                'topicfk': legalbase.topicfk,
-                'title': legalbase.title,
-                'officialtitle': legalbase.officialtitle,
-                'abreviation': legalbase.abreviation,
-                'officialnb': legalbase.officialnb,
-                'canton': legalbase.canton,
-                'commune': legalbase.commune,
-                'documenturl': legalbase.legalbaseurl,
-                'legalstate': legalbase.legalstate,
-                'publishedsince': legalbase.publishedsince.isoformat()
-            }
-        })
-
-    return {'legalbases': doclist}
 
 @view_config(route_name='getDocumentReferences', renderer='json')
 def getDocumentReferences(docfilters):
