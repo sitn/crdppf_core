d927c1817eab9b91e9cbe3fd12741e1f8a1824a6
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2015-04-30 11:21:11 +0200

    Adding the first parameters to print json

--- crdppf/lib/cached_content.py
+++ crdppf/lib/cached_content.py
@@ -4,12 +4,56 @@ from dogpile.cache.region import make_region
 cache_region = make_region()
 cache_region.configure("dogpile.cache.memory")
 
+from crdppf.models import DBSession
+
+from crdppf.models import AppConfig, Translations
+
+import logging
+
+log = logging.getLogger(__name__)
+
 @cache_region.cache_on_arguments()
-def get_cached_content():
+def get_cached_content(request):
+
     d={}
+
+    canton_logo = DBSession.query(AppConfig).filter_by(parameter='cantonlogopath').first()
+
+    d['canton_logo'] = '/'.join([
+        request.registry.settings['localhost_url'],
+        'proj/images',
+        canton_logo.paramvalue
+    ])
+
     return d
-    
+
 @cache_region.cache_on_arguments()
 def get_cached_content_l10n(lang):
+
     d={}
+
+    translations_list = {
+        'legendlabel': 'legend_header_label',
+        'completlegendlabel': 'full_topic_legend_label',
+        'teneur': 'tenor_label',
+        'legalbaseslabel': 'legal_base_label',
+        'legalprovisionslabel': 'legal_disposition_label',
+        'referenceslabel': 'reference_label',
+        'competentauthoritylabel': 'authority_label',
+        'temporaryprovisionslabel': 'transitory_disposition_label',
+        'mapslabel': 'maps_label',
+        'otherslabel': 'other_label',
+    }
+
+    translations = DBSession.query(Translations).filter(Translations.varstr.in_(translations_list)).all()
+
+    for translation in translations:
+
+        varstr = translation.varstr
+
+        if  getattr(translation, lang):
+            d[translations_list[varstr]] = getattr(translation, lang)
+        else:
+            log.warning("There is a undefined translation")
+
     return d
