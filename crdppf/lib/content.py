4c83dcac58d5a7718384edc7db05bc630a31562c
Francois Voisard <francois.voisard@ne.ch>
2016-05-19 14:38:01 +0200

    added more content as pdf template input

--- crdppf/lib/content.py
+++ crdppf/lib/content.py
@@ -7,30 +7,111 @@ from datetime import datetime
 from crdppf.lib.wmts_parsing import wmts_layer
 from crdppf.lib.geometry_functions import get_feature_bbox, get_print_format, get_feature_center
 
-from crdppf.util.pdf_functions import get_feature_info, get_translations
+from crdppf.views.get_features import get_features_function
+from crdppf.views.legal_documents import getDocumentReferences, getLegalDocuments
+from crdppf.util.pdf_functions import get_feature_info, get_translations, get_XML
 
-from crdppf.models import DBSession,Topics
+from crdppf.models import DBSession,Topics,AppConfig
 
 import logging
 
 log = logging.getLogger(__name__)
 
+def set_documents(request, topicid, doctype, docids, featureInfo, geofilter):
+    """ Function to fetch the documents related to the restriction:
+    legal provisions, temporary provisions, references 
+    """
+
+    docs = {}
+    documents = []
+    
+    if geofilter is True:
+        filters = {
+            'docids': docids,
+            'topicid': topicid,
+            'cadastrenb': featureInfo['numcom'],
+            'chmunicipalitynb': featureInfo['nufeco']
+        }
+    else:
+        filters = {'docids':docids}
+
+    if len(docids) > 0:
+        docs = getLegalDocuments(request,filters)
+    else:
+        docs['docs'] = []
+
+    references = []
+    # store the documents in a list
+    for doc in docs['docs']:
+        if doc['doctype'] == doctype and doc['documentid'] in docids:
+            references.append(doc)
+            if doc['doctype'] != u'legalbase' and doc['documentid'] not in self.doclist:
+                self.add_appendix(topicid, 'A'+str(len(self.appendix_entries)+1), unicode(doc['officialtitle']).encode('iso-8859-1'), unicode(doc['remoteurl']).encode('iso-8859-1'), doc['localurl'])
+            if doc['documentid'] not in self.doclist:
+                self.doclist.append(doc)
+        if doc['doctype'] == doctype and geofilter is True and doc['documentid'] not in docids:
+            references.append(doc)
+            if doc['doctype'] != u'legalbase' and doc['documentid'] not in self.doclist:
+                self.add_appendix(topicid, 'A'+str(len(self.appendix_entries)+1), unicode(doc['officialtitle']).encode('iso-8859-1'), unicode(doc['remoteurl']).encode('iso-8859-1'), doc['localurl'])
+            if doc['documentid'] not in self.doclist:
+                self.doclist.append(doc)
+                
+    return references
+        
+def add_toc_entry(topicid, num, label, categorie, appendices):
+    tocentries = {}
+    tocentries[str(topicid)]={'no_page':num, 'title':label, 'categorie':int(categorie), 'appendices':set()}
+    
+    return tocentries
+
+def add_layer(request, layer, featureid, featureInfo, translations, appconfig, topiclist):
+
+    layerlist = {}
+    
+    results = get_features_function(featureInfo['geom'],{'layerList':layer.layername,'id':featureid,'translations':translations})
+
+    if results :
+        layerlist[str(layer.layerid)]={'layername':layer.layername,'features':[]}
+        for result in results:
+            # for each restriction object we check for related documents
+            docfilters = [str(result['id'])]
+            for doctype in appconfig['doctypes']:
+                docidlist = getDocumentReferences(docfilters)
+                result['properties'][doctype] = set_documents(request, str(layer.topicfk), doctype, docidlist, featureInfo, False)
+            layerlist[str(layer.layerid)]['features'].append(result['properties'])
+        dsfg
+        # we also check for documents on the layer level - if there are any results - else we don't need to bother
+        docfilters = [layer.layername]
+        for doctype in appconfig['doctypes']:
+            docidlist = getDocumentReferences(docfilters)
+            topiclist[str(layer.topicfk)]['layers'][layer.layerid][doctype] = set_documents(request, str(layer.topicfk), doctype, docidlist, featureInfo, True)
+
+        topiclist[str(layer.topicfk)]['categorie']=3
+        topiclist[str(layer.topicfk)]['no_page']='tocpg_'+str(layer.topicfk)
+        topiclist[str(layer.topicfk)]['layers'][layer.layerid]['features'] = layerlist[str(layer.layerid)]['features']
+    else:
+        layerlist[str(layer.layerid)]={'layername':layer.layername,'features':None}
+        if topiclist[str(layer.topicfk)]['categorie'] != 3:
+            topiclist[str(layer.topicfk)]['categorie']=1
+    weer
+    return topiclist
+    
 def get_content(idemai, request):
     """ TODO....
         Explain how the whole thing works...
     """
     # Start a session
     session = request.session
-     
-    extractcreationdate = datetime.now().strftime("%Y%m%d%H%M%S")
+    configs = DBSession.query(AppConfig).all()
     
-    extract = {
-        'pdfid': extractcreationdate,
-        'translations': {},
-        'featureInfo':{},
-        'topics':{}
-        }
-        
+    appconfig = {}
+    for config in configs:
+        if not config.parameter in [u'crdppflogopath', u'cantonlogopath']:
+            appconfig[config.parameter] = config.paramvalue
+    extract = {}
+    extractcreationdate = datetime.now().strftime("%Y%m%d%H%M%S")
+    extract['pdfid'] = extractcreationdate
+
     # Define language to get multilingual labels for the selected language
     # defaults to 'fr': french - this may be changed in the appconfig
     if 'lang' not in session:
@@ -38,7 +119,8 @@ def get_content(idemai, request):
     else : 
         lang = session['lang'].lower()
     translations = get_translations(lang)
-    extract['translations'] = translations
+    appconfig['translations'] = translations
+
     
     # 1) If the ID of the parcel is set get the basic attributs of the property
     # else get the ID (idemai) of the selected parcel first using X/Y coordinates of the center 
@@ -49,21 +131,6 @@ def get_content(idemai, request):
     # 3) Get the list of all the restrictions by topicorder set in a column
     #-------------------------------------------
     topics = DBSession.query(Topics).order_by(Topics.topicorder).all()
-    
-    for topic in extract['topics']:
-            # for the federal data layers we get the restrictions calling the feature service and store the result in the DB
-            if topic.topicid in extract.appconfig.ch_topics:
-                xml_layers = []
-                for xml_layer in topic.layers:
-                    xml_layers.append(xml_layer.layername)
-                get_XML(extract.featureInfo['geom'], topic.topicid, pdfconfig.timestamp, lang, translations)
-            
-    for topic in topics:
-        extract['topics'][topic.topicid] = {
-            'topicname': topic.topicname,
-            'topicorder': topic.topicorder
-            }
-    sdf
     
     # Configure the WMTS background layer
     url = request.registry.settings['wmts_getcapabilities_url']
@@ -107,7 +174,7 @@ def get_content(idemai, request):
 
     for char in conversion:
         municipality_escaped = municipality_escaped.replace(char[0], char[1])
-        
+
     municipalitylogodir = '/'.join([
         request.registry.settings['localhost_url'],
         'proj/images/ecussons/'])
@@ -178,21 +245,69 @@ def get_content(idemai, request):
 
     #~ base_map = deepcopy(map)
     #~ base_map["bbox"] = " "
+        
+    data = []
+    topiclist = {}
 
-    sddfkh
-    data = [
-        {
-          "topic_title": "bli",
-          "topic_text": "bla",
-          "map": map
-          },
-        {
-          "topic_title": "blu",
-          "topic_text": "blo",
-          "map": map
-          }
-        ]
-    
+    for topic in topics:
+        layers = []
+
+        # for the federal data layers we get the restrictions calling the feature service and store the result in the DB
+        #~ if topic.topicid in appconfig['ch_topics']:
+            #~ xml_layers = []
+            #~ for xml_layer in topic.layers:
+                #~ xml_layers.append(xml_layer.layername)
+            #~ get_XML(featureInfo['geom'], topic.topicid, extractcreationdate, lang, translations)
+
+        topiclist[str(topic.topicid)]={
+            'categorie':0,
+            'topicname':topic.topicname,
+            'layers':{},
+            'authority':topic.authority, 
+            'topicorder':topic.topicorder,
+            'authorityfk':topic.authorityfk,
+            'publicationdate':topic.publicationdate
+            }
+
+        toc_entries = {}
+        # if geographic layers are defined for the topic, get the list of all layers and then
+        # check for each layer the information regarding the features touching the property
+        if topic.layers:
+            add_toc_entry(topic.topicid, '', str(topic.topicname.encode('iso-8859-1')), 1, '')
+            for layer in topic.layers:
+                topiclist[str(topic.topicid)]['layers'][layer.layerid]={
+                    'layername':layer.layername,
+                    'features':None
+                    }
+                # intersects a given layer with the feature and adds the results to the topiclist- see method add_layer
+                toto = add_layer(request, layer, propertynumber, featureInfo, translations, appconfig, topiclist)
+                sdf
+                topiclist.update(add_layer(request, layer, propertynumber, featureInfo, translations, appconfig, topiclist))
+            #~ get_topic_map(topic.layers, topic.topicid)
+            # Get the list of documents related to a topic with layers and results
+            if topiclist[str(layer.topicfk)]['categorie'] == 3:
+                docfilters = [str(topic.topicid)]
+                for doctype in appconfig['doctypes']:
+                    docidlist = getDocumentReferences(docfilters)
+                    topiclist[str(topic.topicid)][doctype] = set_documents(request, str(topic.topicid), doctype, docidlist, featureInfo, True)      
+
+        else:
+            if str(topic.topicid) in appconfig['emptytopics']:
+                topiclist[str(topic.topicid)]['layers'] = None
+                topiclist[str(topic.topicid)]['categorie'] = 1
+            else:
+                topiclist[str(topic.topicid)]['layers'] = None
+                topiclist[str(topic.topicid)]['categorie'] = 0
+        wer
+        data.append(
+            {
+            'topic_title': topic.topicname,
+            'topic_text': topic.topicorder,
+            'topic_layers': layers,
+            #'topiclist': topiclist,
+            'map': map
+            })
+
     d = {
     #    "datasource": [],
         "map": map
