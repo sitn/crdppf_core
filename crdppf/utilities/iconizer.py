5026589266ee2de8797230da54aa569ea4bcbf1a
Francois Voisard <francois.voisard@ne.ch>
2017-02-28 16:10:18 +0100

    intermediate state symbol generator script

--- crdppf/utilities/iconizer.py
+++ crdppf/utilities/iconizer.py
@@ -5,14 +5,20 @@ FileName: iconizer.py
 Creates all the symbols to generate the dynamic legend for the PDF extract
 =============================================================================
 Created: [2016/12/19]
+Updated: [2017/02/13]
 Author: François Voisard
 """
 
 import os.path
+import pkg_resources
 import argparse
 import yaml
 
+import httplib2
+from urlparse import urlparse
+
 from pyramid.paster import get_app
+from xml.dom.minidom import parseString
 
 def main():
     """ Script to parse all the icons for the legends of the public land restrictions application
@@ -42,10 +48,10 @@ def main():
     app_config = options.app_config
     db_config = options.db_config
     pdf_config = options.pdf_config
-
+    
     if not os.path.isfile(app_config):
         parser.error("Cannot find config file: {0!s}".format(app_config))
-        
+    
     if not os.path.isfile("config_db.yaml"):
         parser.error("Cannot find the database config file: {0!s}".format(db_config))
         exit(1)
@@ -55,143 +61,156 @@ def main():
     if not os.path.isfile("config_pdf.yaml"):
         parser.error("Cannot find the pdf config file: {0!s}".format(pdf_config))
         exit(1)
-
     with open("config_pdf.yaml", "r") as pdf_config:
         pdfconfig = yaml.load(pdf_config)
     
     # Ignores pyramid deprecation warnings
     #~ warnings.simplefilter("ignore", DeprecationWarning)
 
-    get_app(app_config)
-    from crdppf.models import DBSession, Layers
+    myapp = get_app(app_config)
+    configs = {}
+    
+    for key, value in myapp.registry.settings.iteritems():
+        configs[key] = value
+        if key == 'app_config':
+            for k, v in configs['app_config'].iteritems():
+                configs[k] = v
+        if key == 'pdf_config':
+            for k, v in configs['pdf_config'].iteritems():
+                configs[k] = v
+     
+    wmsconfigs = set_wms_config(configs)
+    
+    from crdppf.models import DBSession, Topics, Layers
     
     session = DBSession()
+    topics = session.query(Topics).all()
     layers = session.query(Layers).all()
     for layer in layers:
-        iconizer(layer)
+        if layer.baselayer == False and layer.layername not in ['r132_perimetre_prot_eau','r131_zone_prot_eau']:
+            iconizer(layer, wmsconfigs, configs)
 
 
-def iconizer(restriction_layer):
-    """ script to create a symbol 
+def set_wms_config(configs):
+    """ Sets the basic WMS parameters in function of the topic
     """
 
-    print restriction_layer.layername
+    wmsurl = configs ['crdppf_wms'] 
+    wms_get_styles = {
+        'REQUEST': 'GetStyles'
+        }
+    wms_get_legend = {
+        'REQUEST': 'GetLegendGraphic',
+        'TRANSPARENT': configs['wms_transparency']
+    }
+    wms_params = {
+        'SERVICE': 'WMS',
+        'VERSION': str(configs['wms_version']) ,
+        'SRS': str(configs['wms_srs']),
+        'lang':configs['lang']
+        }
+    for key, value in wms_params.iteritems():
+        wms_get_legend[key] = value
+        wms_get_styles[key] = value
     
-    return
+    return {'baseurl': wmsurl, 'getlegend': wms_get_legend, 'getstyles': wms_get_styles}
 
-def iconizer_copy(restriction_layers):
-        # Adding each layer of the restriction to the WMS
-    for restriction_layer in restriction_layers:
-        legend_layers.append(restriction_layer.layername)
-        layers.append(restriction_layer.layername)
-        self.wms_get_legend ['LAYER'] = restriction_layer.layername
-        self.wms_get_styles['LAYERS'] = restriction_layer.layername
-        legend = open(self.appconfig.tempdir+self.filename+str('_legend_')+str(restriction_layer.layername)+'.png', 'wb')
-        self.cleanupfiles.append(self.appconfig.tempdir+self.filename+str('_legend_')+str(restriction_layer.layername)+'.png')
-        legend_path.append(self.appconfig.tempdir+self.filename+str('_legend_')+str(restriction_layer.layername))
 
-        # gets a list of all the categories of objects found in the map perimeter to reduce the legend
-        legend_classes = set(self.get_legend_classes(wmsbbox,restriction_layer.layername))
-        self.wms_get_legend['FORMAT'] = 'image/png'
-        self.wms_get_legend['TRANSPARENT'] = self.wms_transparency
+def iconizer(restriction_layer, wmsconfigs, configs):
+    """ script to create a symbol for each class of the PLR map layers
+    """
+    
+    layers = []
+    
+    tempdir = pkg_resources.resource_filename('crdppfportal', 'static/public/temp_files/')
+    iconsdir = str(configs['imagesbasedir'])+str('/icons')
+    sld_url = configs ['localhost_url'] +str('/proj/public/temp_files/')
+    
+    # compile and call getStyles query
+    getstylesurl = wmsconfigs['baseurl']
+    wms_get_styles =wmsconfigs['getstyles']
+    wms_get_styles['LAYERS'] = restriction_layer.layername
 
-        getstylesurl = self.wms_url
+    if getstylesurl.find('?') < 0:
+        getstylesurl += '?'
+    getstylesurl = getstylesurl + '&'.join(['%s=%s' % (key, value) for (key, value) in wms_get_styles.items()])
 
-        if getstylesurl.find('?') < 0:
-            getstylesurl += '?'
-        getstylesurl = getstylesurl + '&'.join(['%s=%s' % (key, value) for (key, value) in self.wms_get_styles.items()])
+    http = httplib2.Http()
 
-        http = httplib2.Http()
+    #~ h = dict(request.headers)
+    #~ if urlparse(getstylesurl).hostname != 'localhost': 
+        #~ h.pop('Host')
 
-        h = dict(self.request.headers)
-        if urlparse(getstylesurl).hostname != 'localhost': 
-            h.pop('Host')
+    try:
+        resp, content = http.request(getstylesurl, method='GET')
+    except:
+        if log:
+            log.error("Unable to do GetStyles request for url %s" % getstylesurl)
+        return None
 
-        if self.log:
-            self.log.warning("WMS REQUEST")
-            self.log.warning("on URL: %s", getstylesurl)
-            self.log.warning('Doing layer: %s', restriction_layer.topicfk)
+    origin = parseString(content)
+    rules = origin.getElementsByTagName("Rule")
+    sld_legendfile = open(tempdir+str(restriction_layer.layername)+'_origin_legend_sld.xml', 'w')
+    sld_legendfile.write(origin.toxml("utf-8"))
+    sld_legendfile.close()
 
-        try:
-            resp, content = http.request(getstylesurl, method='GET', headers=h)
-        except:
-            if self.log:
-                self.log.error("Unable to do GetStyles request for url %s" % getstylesurl)
-            return None
-
-        if self.log:
-            self.log.warning("DONE WMS REQUEST")
-
-        dom = parseString(content)
-        rules = dom.getElementsByTagName("Rule")
-
-        complet_list = []
-        # Remove all the classes from the xml which do not appear in the map extract
+    if rules.length > 0:
         for rule in rules:
+            dom = parseString(content)
+            try: 
+                fts = dom.getElementsByTagName("FeatureTypeStyle")[0]
+                fts_up = fts.parentNode
+                fts_up.removeChild(fts)
+                fts_up.appendChild(dom.createElement("FeatureTypeStyle"))
+                fts = dom.getElementsByTagName("FeatureTypeStyle")[0]
+                fts.appendChild(rule)
+            except:
+                pass
+            #~ print dom.toxml("utf-8")
+
             if len(rule.getElementsByTagName("ogc:Literal")) > 0:
                 literal = rule.getElementsByTagName("ogc:Literal")[0]
+                class_name = rule.getElementsByTagName("Name")[0].firstChild.nodeValue
                 literal_value = literal.firstChild.nodeValue
-                complet_list.append(literal_value)
-                if literal_value not in legend_classes:
-                    dynamic_legend = rule.parentNode
-                    dynamic_legend.removeChild(rule)
+                if literal_value == '.':
+                    literal_value = 'autre_'+str(restriction_layer.layername)
+                literal_value.replace(' ','_')
+                #~ # write an sld file with all classes to create an icon for each class
+                sld_legendfile = open(tempdir+str(literal_value)+'_legend_sld.xml', 'w')
+                sld_legendfile.write(dom.toxml("utf-8"))
+                sld_legendfile.close()
+                
+                # compile and call getLegendGraphic query
+                wms_get_legend =wmsconfigs['getlegend']
+                wms_get_legend['FORMAT'] = 'image/png'
+                wms_get_legend['LAYER'] = restriction_layer.layername
+                wms_get_legend['RULE'] = class_name
 
-        # write an sld file to filter the getLegendGraphic request with
-        sld_legendfile = open(self.appconfig.tempdir+self.filename+str('_')+str(restriction_layer.layername)+'_legend_sld.xml', 'w')
-        self.cleanupfiles.append(self.appconfig.tempdir+self.filename+str('_')+str(restriction_layer.layername)+'_legend_sld.xml')
-        sld_legendfile.write(dom.toxml("utf-8"))
-        sld_legendfile.close()
+                if 'SLD' in wms_get_legend:
+                    del wms_get_legend['SLD']
 
-        # only necessary if complet legend should be called dynamically
-        #complet_legend_body = urllib.urlencode(self.wms_get_legend)
+                legend_sld = sld_url+str(literal_value)+'_legend_sld.xml'
+                wms_get_legend['SLD'] = str(legend_sld)
 
-        if self.log:
-            self.log.warning("Applying SLD")
+                getsldurl = wmsconfigs['baseurl']
 
-        if 'SLD' in self.wms_get_legend:
-            del self.wms_get_legend['SLD']
+                if getsldurl.find('?') < 0:
+                    getsldurl += '?'
+                getsldurl = getsldurl + '&'.join(['%s=%s' % (key, value) for (key, value) in wms_get_legend.items()])
+                print getsldurl
 
-        if self.log:
-            self.log.warning("DONE Applying SLD")
+                http = httplib2.Http()
 
-        if sld_legendfile and topicid in [u'R073','R073','73',u'73']:
-            legend_sld = self.sld_url+self.filename+str('_')+str(restriction_layer.layername)+'_legend_sld.xml'
-            self.wms_get_legend['SLD'] = str(legend_sld)
+                try:
+                    resp, content = http.request(getsldurl, method='GET')
+                except:
+                    if log:
+                        log.error("Unable to do GetLegend request for url %s" % getsldurl)
+                    return None
 
-        getsldurl = self.wms_url
+            else:
+                return
 
-        if getsldurl.find('?') < 0:
-            getsldurl += '?'
-        getsldurl = getsldurl + '&'.join(['%s=%s' % (key, value) for (key, value) in self.wms_get_legend.items()])
-
-        http = httplib2.Http()
-
-        h = dict(self.request.headers)
-        if urlparse(getsldurl).hostname != 'localhost':
-            h.pop('Host')
-
-        try:
-            resp, content = http.request(getsldurl, method='GET', headers=h)
-        except:
-            if self.log:
-                self.log.error("Unable to do GetMap request for url %s" % getsldurl)
-            return None
-
-        if topicid in self.appconfig.ch_topics:
-            front = Image.open(StringIO(content))
-            front = front.point(lambda x: x*0.7)
-            legend = Image.new('RGBA', front.size, (255, 255, 255))
-            legend.paste(front, (0, 0), front)
-            legend = legend.convert('RGB')
-            legend.save(self.appconfig.tempdir+self.filename+str('_legend_')+str(topicid)+'.png')
-        else :
-            legend.write(content)
-            legend.close()
-
-        if self.log:
-            self.log.warning("DONE SLD on WMS")
-
-    self.topiclist[topicid]['topiclegend'] = self.topiclegenddir+str(topicid)+'_topiclegend.pdf'
 
     return
 
