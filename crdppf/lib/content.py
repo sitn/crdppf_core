104c2751582a0e3ce7a1c529f4cd39622fed7773
Francois Voisard <francois.voisard@ne.ch>
2017-11-29 15:05:17 +0100

    flaked lib folder

--- crdppf/lib/content.py
+++ crdppf/lib/content.py
@@ -26,7 +26,7 @@ def get_mapbox(feature_center, scale, height, width, fitratio):
         scale: the map scale denominator
         feature_center: center point (X/Y) of the real estate feature
     """
-    
+
     scale = scale*1.1
     delta_Y = round((height*scale/1000)/2, 1)
     delta_X = round((width*scale/1000)/2, 1)
