33eb3a7aa69a6305360595bbe2ae8499924f0715
Francois Voisard <francois.voisard@ne.ch>
2017-12-07 09:55:35 +0100

    added pdf archiving functionality

--- crdppf/views/printproxy.py
+++ crdppf/views/printproxy.py
@@ -113,9 +113,16 @@ class PrintProxy(Proxy):  # pragma: no cover
     def report_get(self):
         """ Get the PDF. """
 
-        return self._proxy_response(
+        archive_path = self.config['pdf_archive_path']
+
+        pdf = self._proxy_response(
             "%s/report/%s" % (
                 self.config['print_url'],
                 self.request.matchdict.get('ref')
             ),
         )
+        filename = pdf.content_disposition.split('=')[1]
+        with open(archive_path+filename, 'wb') as f:
+            f.write(pdf.body)
+        
+        return pdf
