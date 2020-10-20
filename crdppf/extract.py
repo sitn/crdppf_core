5436d47b7214063d5edd35b08fb5548ab43ccc4e
Francois Voisard <francois.voisard@ne.ch>
2020-10-20 16:54:01 +0200

    extended extract class with simplified functionality

--- crdppf/extract.py
+++ crdppf/extract.py
@@ -1,10 +1,18 @@
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
+from sqlalchemy import or_
+
 from crdppf import db_config
 
 from pyramid.httpexceptions import HTTPBadRequest, HTTPNotFound
 from datetime import datetime
 
+from crdppfportal.table2model_match import table2model_match
+
 from crdppf.util.pdf_functions import get_translations, get_feature_info
+from crdppf.util.get_feature_functions import get_features_function
+
+from crdppf.models import DBSession
+from crdppf.models.models import Topics, Layers, OriginReference
 
 # MAIN DOCUMENT
 class Extract(object):
@@ -87,3 +95,109 @@ class Extract(object):
             self.real_estate = get_feature_info(self.propertyid, self.srid, self.translations)
         except:
             raise HTTPNotFound('No real estate with the given id could be found.')
+
+        #Ttry:
+        self.topics = self.get_topics()
+        #except:
+        #    raise HTTPNotFound('There is an error regarding the topics for this municipality.')
+
+        #try:
+        self.layers = self.get_layers()
+        #except:
+        #    raise HTTPNotFound('An error occured fetching the PLR relevant layers.')
+
+        self.restrictions = self.get_restrictions()
+
+        self.references = self.get_references()
+
+
+    def get_topics(self):
+        """ Get the list of PLR topics
+        """
+
+        topics = DBSession.query(Topics).order_by(Topics.topicorder).all()
+        if len(topics) > 0:
+            topiclist = {}
+            for topic in topics:
+                topiclist[topic.topicorder] = {
+                    'topicid': topic.topicid,
+                    'topicname': topic.topicname,
+                    'publicationdate': topic.publicationdate,
+                    'authority': topic.authority,
+                    'layers': topic.layers
+                }
+        else:
+            raise HTTPBadRequest('No topic was found.')
+
+        return topiclist
+
+    def get_layers(self):
+        """ Get the list of all PLR layers
+        """
+
+        layers = DBSession.query(Layers).filter(Layers.baselayer.is_(False)).order_by(Layers.layerid).all()
+        if len(layers) > 0:
+            layerlist = []
+            for layer in layers:
+                layerlist.append({
+                    'layerid': layer.layerid,
+                    'topicid': layer.topicfk,
+                    'topicorder': layer.topic.topicorder,
+                    'layername': layer.layername,
+                    'publicationdate': layer.publicationdate
+                })
+
+        return layers
+
+    def get_restrictions(self):
+        """ For each available topic, get the restrictions
+        """
+
+        parcelGeom = self.real_estate['geom']
+
+        if len(self.layers) > 0:
+            restrictionlist = []
+            for layer in self.layers:
+                results = get_features_function(parcelGeom, {'layerList': layer.layername})
+                if len(results) > 0:
+                    for result in results:
+                        restriction = {
+                            'topicid': layer.topic.topicid,
+                            'topicname': layer.topic.topicname,
+                            'layername': layer.layername,
+                            'restrictionid': result['id'],
+                            'teneur': result['properties']['teneur'],
+                            'codegenre': result['properties']['codegenre'],
+                            'statutjuridique': result['properties']['statutjuridique'],
+                            'datepublication': result['properties']['datepublication'],
+                            'intersectionMeasure': result['properties']['intersectionMeasure'],
+                            'geomType': result['properties']['geomType']
+                        }
+                        restrictionlist.append(restriction)
+                del(results)
+        else:
+            raise HTTPBadRequest('No layer found for this restriction.')
+
+        return restrictionlist
+
+    def get_references(self):
+        """ For each restriction get the references
+        """
+        if len(self.restrictions) > 0:
+            for restriction in self.restrictions:
+                references = DBSession.query(OriginReference).filter(OriginReference.fkobj==restriction['restrictionid']).order_by(OriginReference.docid).all()
+
+                if len(references) > 0:
+                    referencelist = []
+                    for reference in references:
+                        referencelist.append({
+                            'docid': reference.docid,
+                            'officialtitle': reference.documents.officialtitle,
+                            'officialnb': reference.documents.officialnb,
+                            'remoteurl': reference.documents.remoteurl
+                        })
+                restriction.update({'references': referencelist})
+
+            sdf
+
+        return referencelist
